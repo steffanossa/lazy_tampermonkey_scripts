dec6731c3b4e385ea7ffd2f7f7ad9a9598ac2903
steffanossa <94658723+steffanossa@users.noreply.github.com>
2025-07-14 23:27:03 +0200

    Update Searchbar_Autofocus.js

--- Searchbar_Autofocus.js
+++ Searchbar_Autofocus.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Searchbar Autofocus
-// @version      0.1
-// @description  Focus the search bars with Ctrl + Space, blur with Escape. Mostly taken from https://addons.mozilla.org/en-US/firefox/user/16991196/?utm_source=firefox-browser&utm_medium=firefox-browser&utm_content=addons-manager-user-profile-link
+// @version      0.2
+// @description  Focus the search bars with Ctrl + Space, blur with Escape.
 // @author       steffanossa
 // @match        *://*/*
 // @grant        none
@@ -32,11 +32,15 @@
     if (textInpEls.length === 0) return null;
 
     const findSearchInput = (element) => {
-    if (!element || !(element.type === "text" || element.type === "search")) return null;
+    if (!element || !(element.type === "text" || element.type === "search")) {
+      return null;
+    }
     for (const attribute of element.attributes) {
       const formatAttr = attribute.value.toLowerCase();
-      if (formatAttr.includes("disabled") || formatAttr.includes("hidden")) continue;
-      if (formatAttr.includes("search") || formatAttr === "q" || formatAttr.includes("such") || formatAttr.includes("cherch")) {
+      if (formatAttr.includes("disabled") || formatAttr.includes("hidden")) {
+        continue;
+      }
+      if (formatAttr.includes("search") || formatAttr === "q" || formatAttr.includes("such") || formatAttr.includes("cherch") || formatAttr.includes("busca")) {
         return element;
       }
     }
